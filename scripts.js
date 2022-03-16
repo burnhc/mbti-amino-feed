a772fdff0ed9c58b3bd4cae0bcea4a23780a5993
Chandra <burnhc@uw.edu>
2022-03-15 19:38:29 -0700

    testing

--- scripts.js
+++ scripts.js
@@ -53,8 +53,8 @@ function printCalendar() {
                 var startsAt = moment(entry.start.date).format(dateFormat);
 
                 summary = summary.replace("🎈", `<i class="fas fa-balloon fa-fw"></i>`);
-                var profileLink = description.slice(9, description.indexOf(`">Go to `));
-                description = description.slice(description.indexOf(`'s profile »</a>` + 16));
+                var profileLink = description.slice(9, description.indexOf(`">Go to `)).replace(/\//g, '\\');
+                description = description.slice(description.indexOf(`</a>` + 4));
                 console.log(profileLink, description)
                 calendarRows.push(
                     `<a href="${profileLink}" class="list-group-item list-group-item-action"><h6 class="mb-1">${startsAt}:</h6><p class="mb-1">${summary}</p><p>${description}</p></a>`
